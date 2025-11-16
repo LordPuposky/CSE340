1aeae02916c6effe442f70030b6bebb45669cce6
Yesid Romero <augustomero@gmail.com>
2025-11-15 22:51:27 -0500

    fix: Simplify index.ejs and use layout pattern

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,16 @@
 const utilities = require("../utilities/")
 const baseController = {}
 
-baseController.buildHome = async function (req, res) {
-    const nav = await utilities.getNav()
-    res.render("index", { title: "Home", nav })
+baseController.buildHome = async function (req, res, next) {
+    try {
+        const nav = await utilities.getNav()
+        res.render("index", { 
+        title: "Home", 
+        nav 
+        })
+    } catch (err) {
+        next(err)
+    }
 }
 
 module.exports = baseController
