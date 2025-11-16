6861d3807b42b2b28a54e5c54f15104cf6a34457
Yesid Romero <augustomero@gmail.com>
2025-11-15 23:02:24 -0500

    fix: Use partials/nav.ejs include in layout

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,9 +4,9 @@ const baseController = {}
 baseController.buildHome = async function (req, res, next) {
     try {
         const nav = await utilities.getNav()
-        res.render("index", { 
-        title: "Home", 
-        nav 
+        res.render("index", {
+        title: "Home",
+        nav
         })
     } catch (err) {
         next(err)
